853b1a41783cc76823afb5fc0060e471e0fad8e2
michael <mickwald@hotmail.com>
2020-09-11 14:26:51 +0200

    Added some basic types, some needs to be converted to classes

--- Global/main.cpp
+++ Global/main.cpp
@@ -1,6 +1,46 @@
 #include <iostream>
+#include <list>
 
 using namespace std;
+
+struct ColorDbl{
+    double r, g, b;
+};
+
+struct Vertex {
+    double x, y, z, w;
+};
+
+struct Direction {
+    double x, y, z;
+};
+
+struct Triangle {           //Convert to class
+    Vertex a, b, c; //names?
+    ColorDbl color;
+    Direction normal;
+};
+
+struct Scene {          //Convert to class
+    Triangle triangles[24];
+    ColorDbl colors[6];
+};
+struct Ray {
+    Vertex start, end;
+    ColorDbl color;
+};
+
+struct Pixel{       //Possibly class
+    ColorDbl r, g, b;
+    Ray intersectingRay;        //Prepare for array/list/pointer
+};
+
+struct Camera{      //Convert to class
+    Vertex leftEye, rightEye;
+    Pixel image[800][800];
+
+};
+
 
 int main() {
     
