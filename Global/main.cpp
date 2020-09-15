63e762fb68465861d862647df506aafc12a8dcae
michael <mickwald@hotmail.com>
2020-09-15 10:30:32 +0200

    Branched imagegeneration

--- Global/main.cpp
+++ Global/main.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
-#include <list>
 
 using namespace std;
 
 struct ColorDbl{
     double r, g, b;
 };
-
+/*
 struct Vertex {
     double x, y, z, w;
 };
@@ -40,10 +39,19 @@ struct Camera{      //Convert to class
     Pixel image[800][800];
 
 };
-
+*/
 
 int main() {
-    
     cout << "This is the start of our RayTracer!" << endl;
+    const int xWidth = 1920;
+    const int yWidth = 1080;
+    string filename = "Test.png";
+    int* image = new int(yWidth*xWidth);
+    for(int i = 0; i < yWidth; i++){
+        for(int j = 0; j < xWidth; j++){
+            image[i*xWidth+j] = 2;//255*(i+j)/(xWidth+yWidth);//ColorDbl{(double)(255*i/yWidth), (double)(255*j/xWidth), (double)(255*(i+j)/(xWidth+yWidth))};
+        }
+    }
+    cout << "Generated an image!" << endl;
     return 0;
 }
